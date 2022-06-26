9be64a5c5c2873c1e03e9b4b6699fa0d4433654e
Yin Guanhao <sopium@mysterious.site>
2022-06-26 11:34:25 +0800

    deny clippy warnings

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 set -ex
 
-cargo clippy --all --no-default-features
-cargo clippy --all --no-default-features --features=client
-cargo clippy --all --features=client
-cargo clippy --all --features=blocking-client
-cargo clippy --all --all-targets --all-features
+cargo clippy --all --no-default-features -- -D warnings
+cargo clippy --all --no-default-features --features=client -- -D warnings
+cargo clippy --all --features=client -- -D warnings
+cargo clippy --all --features=blocking-client -- -D warnings
+cargo clippy --all --all-targets --all-features -- -D warnings
 cargo +nightly test --all --all-features --all-targets
